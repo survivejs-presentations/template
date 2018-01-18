242922d93fafcf83559cca198c8a78f7c7f745fc
Juho Vepsalainen <bebraw@gmail.com>
2018-01-18 16:16:37 +0100

    chore: Apply Prettier

--- webpack.config.js
+++ webpack.config.js
@@ -4,77 +4,85 @@ const webpack = require("webpack");
 
 const PATHS = {
   presentation: [
-    path.join(__dirname, 'index.js'),
-    path.join(__dirname, 'images', 'index.js'),
-    path.join(__dirname, 'presentation'),
-    path.join(__dirname, 'node_modules', 'react-tweet-embed')
-  ]
+    path.join(__dirname, "index.js"),
+    path.join(__dirname, "images", "index.js"),
+    path.join(__dirname, "presentation"),
+    path.join(__dirname, "node_modules", "react-tweet-embed"),
+  ],
 };
 
 module.exports = {
   devtool: "source-map",
-  entry: [
-    "webpack-hot-middleware/client",
-    "babel-polyfill",
-    "./index"
-  ],
+  entry: ["webpack-hot-middleware/client", "babel-polyfill", "./index"],
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/dist/"
+    publicPath: "/dist/",
   },
   resolve: {
-    extensions: ['.jsx', '.js']
+    extensions: [".jsx", ".js"],
   },
   resolveLoader: {
     alias: {
-      content: path.join(__dirname, 'loaders', 'content')
-    }
+      content: path.join(__dirname, "loaders", "content"),
+    },
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
   module: {
-    loaders: [{
-      test: /\.md$/,
-      loader: "html-loader!markdown-loader?gfm=false"
-    }, {
-      test: /\.(js|jsx)$/,
-      loader: "babel-loader",
-      query: {
-        plugins: [
-          [
-            "react-transform", {
-              transforms: [{
-                transform: "react-transform-hmr",
-                imports: ["react"],
-                locals: ["module"]
-              }, {
-                transform: "react-transform-catch-errors",
-                imports: ["react", "redbox-react"]
-              }]
-            }
-          ]
-        ]
+    loaders: [
+      {
+        test: /\.md$/,
+        loader: "html-loader!markdown-loader?gfm=false",
       },
-      include: PATHS.presentation
-    }, {
-      test: /\.css$/,
-      loaders: ["style-loader", "raw-loader"],
-      include: __dirname
-    }, {
-      test: /\.gif$/,
-      loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-    }, {
-      test: /\.svg$/,
-      loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-    }, {
-      test: /\.png$/,
-      loader: "url-loader?limit=10000mimetype=image/png"
-    }, {
-      test: /\.jpg$/,
-      loader: "url-loader?limit=10000mimetype=image/jpg"
-    }]
-  }
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        query: {
+          plugins: [
+            [
+              "react-transform",
+              {
+                transforms: [
+                  {
+                    transform: "react-transform-hmr",
+                    imports: ["react"],
+                    locals: ["module"],
+                  },
+                  {
+                    transform: "react-transform-catch-errors",
+                    imports: ["react", "redbox-react"],
+                  },
+                ],
+              },
+            ],
+          ],
+        },
+        include: PATHS.presentation,
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style-loader", "raw-loader"],
+        include: __dirname,
+      },
+      {
+        test: /\.gif$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
+      },
+      {
+        test: /\.svg$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader?limit=10000mimetype=image/png",
+      },
+      {
+        test: /\.jpg$/,
+        loader: "url-loader?limit=10000mimetype=image/jpg",
+      },
+    ],
+  },
 };
